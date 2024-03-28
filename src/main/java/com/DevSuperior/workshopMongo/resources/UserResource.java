8ba5a0f4f16bc955bce9a57794d050dcbe746d27
jlamancio <amancio.jluis@outlook.com>
2024-03-28 09:18:48 -0300

    SpringBoot_MongoDB - Atualizcao de usuário com PUT

--- src/main/java/com/DevSuperior/workshopMongo/resources/UserResource.java
+++ src/main/java/com/DevSuperior/workshopMongo/resources/UserResource.java
@@ -1,6 +1,5 @@
 package com.DevSuperior.workshopMongo.resources;
 
-
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -27,7 +26,6 @@ public class UserResource<T> {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<UserDTO>> findAll() {
-		
 		List<User> list = service.findAll();
 		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
@@ -36,30 +34,33 @@ public class UserResource<T> {
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<UserDTO> findByid(@PathVariable String id) {
-		
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO)  {
-		
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO) {
 		User obj = service.fromDTO(objDTO);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
-		
+
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable String id) {
-		
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+		
 	}
-	
-	
-	
-	
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDTO, @PathVariable String id) {
+		User obj = service.fromDTO(objDTO);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+		
+	}
 }
