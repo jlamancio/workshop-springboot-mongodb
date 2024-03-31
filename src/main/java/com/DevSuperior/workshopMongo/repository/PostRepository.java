0b7b2a2b4c6be4dea28476ba74b321915b3eeedc
jlamancio <amancio.jluis@outlook.com>
2024-03-31 08:39:20 -0300

    SpringBoot_MongoDB - Consulta simples com @Query

--- src/main/java/com/DevSuperior/workshopMongo/repository/PostRepository.java
+++ src/main/java/com/DevSuperior/workshopMongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.DevSuperior.workshopMongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.DevSuperior.workshopMongo.domain.Post;
@@ -10,6 +11,9 @@ import com.DevSuperior.workshopMongo.domain.Post;
 @Repository
 
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }") 
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
